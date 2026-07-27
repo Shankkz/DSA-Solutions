69622bebd4ff797ad5d276ae063a55c17eaa0bdb
Shankz <127765412+Shankkz@users.noreply.github.com>
2026-07-27 12:49:39 +0530

    Time: 45 ms (25.00%) | Memory: 97 MB (58.14%) - LeetSync

--- 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
@@ -1,11 +1,12 @@
 class Solution {
     public long maxProduct(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Math.abs(nums[i]);
+        int j =0;
+        for(int i : nums){
+            nums[j] = Math.abs(i);
+            j++;
         }
-
         Arrays.sort(nums);
-
-        return (long) nums[nums.length - 1] * nums[nums.length - 2] * 100000;
+        
+        return (long) nums[nums.length-1]*nums[nums.length-2]*100000;
     }
 }
