6797933d1c2d7485a669d138f160e7982d372876
Shankz <127765412+Shankkz@users.noreply.github.com>
2026-07-28 09:34:51 +0530

    Time: 68 ms (98.62%) | Memory: 20.7 MB (91.71%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        n = len(s)
+        feq = Counter(s[:n >>1])
+
+        half = "".join(c*feq[c] for c in ascii_lowercase)
+        mid = s[n>>1] if n & 1 else ""
+        return half + mid + half[::-1]
